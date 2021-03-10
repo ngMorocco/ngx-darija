aede9da817c6691f07eec2d832fe0bcf327cdd3c
Slimane IKOURFALN <slimane.ikourfaln@outlook.com>
2021-03-10 23:55:55 +0100

    refactor/enhance Youtube api service

---
+++ src/app/_core/models/index.ts
@@ -0,0 +1,2 @@
+export * from './models';
+export * from './youtube-api-v3.models';
--- src/app/_core/services/youtube-data.service.ts
+++ src/app/_core/services/youtube-data.service.ts
@@ -3,28 +3,30 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
-import { YtVideoItem } from '../models/models';
+import { YtVideoItem, PlaylistItemListResponse } from '../models';
+
+const YT_ROOT_API = 'https://www.googleapis.com/youtube/v3/';
+const ANGULAR_IN_DARIJA_PLAYLIST_ID = 'PLTCFcpZfnDoJxDofsnsNvvV_5djpEl4Bs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeDataService {
 
-  private rootApi = 'https://www.googleapis.com/youtube/v3/';
   private apiKey = environment.googleApiKey;
 
   constructor(private http: HttpClient) { }
 
   getAngularInDarijaVideos(): Observable<YtVideoItem[]> {
-    return this.http.get<any>(`${this.rootApi}playlistItems?part=snippet&maxResults=50&playlistId=PLTCFcpZfnDoJxDofsnsNvvV_5djpEl4Bs&key=${this.apiKey}`)
+    return this.http.get<PlaylistItemListResponse>(`${YT_ROOT_API}playlistItems?part=snippet&maxResults=50&playlistId=${ANGULAR_IN_DARIJA_PLAYLIST_ID}&key=${this.apiKey}`)
     .pipe(
-      map(res => (res.items as Array<any>).map(x => {
+      map(res => res.items!.map(x => {
         return {
-          videoId: x.snippet.resourceId.videoId,
-          title: x.snippet.title,
-          description: x.snippet.description,
-          publishedAt: x.snippet.publishedAt,
-          thumbnailUrl: x.snippet.thumbnails.standard.url
+          videoId: x.snippet!.resourceId!.videoId,
+          title: x.snippet!.title,
+          description: x.snippet!.description,
+          publishedAt: x.snippet!.publishedAt,
+          thumbnailUrl: x.snippet!.thumbnails!.standard!.url
         } as YtVideoItem;
       }))
     );
