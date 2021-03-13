852884b0ae39d255fc215ff1562c0e99b185476e
Slimane IKOURFALN <slimane.ikourfaln@outlook.com>
2021-03-13 18:51:46 +0100

    feat: create a standalone component for responsive YT video player

---
+++ src/app/_shared/components/yt-video-player/yt-video-player.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-yt-video-player',
+  templateUrl: './yt-video-player.component.html',
+  styleUrls: ['./yt-video-player.component.scss'],
+  host: {
+    'class': 'app-yt-video-player'
+  },
+  encapsulation: ViewEncapsulation.None
+})
+export class YtVideoPlayerComponent implements OnInit {
+
+  @Input() videoId: string | undefined = undefined;
+
+  playerVars: YT.PlayerVars = {
+    rel: YT.RelatedVideos.Hide
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
