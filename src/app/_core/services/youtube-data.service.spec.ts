f0d0ca0f73f093404370cce9365ba35f5a2cda04
AhmedLoud <ahmed.loudghiri@gmail.com>
2021-03-13 14:15:17 +0100

    test: fixed tests and removed unrelevant one

--- src/app/_core/services/youtube-data.service.spec.ts
+++ src/app/_core/services/youtube-data.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { YoutubeDataService } from './youtube-data.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('YoutubeDataService', () => {
   let service: YoutubeDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(YoutubeDataService);
   });
 
