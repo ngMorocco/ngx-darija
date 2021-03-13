332f310381df150b223dfb48475243de96021012
AhmedLoud <ahmed.loudghiri@gmail.com>
2021-03-13 13:33:16 +0100

    refactor: add app routing and lazy load home feature

---
+++ src/app/home/home-base/home-base.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home-base',
+  templateUrl: './home-base.component.html'
+})
+export class HomeBaseComponent {
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { YouTubePlayerModule } from '@angular/youtube-player';
-import { VideoBannerComponent } from './video-banner/video-banner.component';
-import { VideoItemComponent } from './video-item/video-item.component';
-import { VideoListingComponent } from './video-listing/video-listing.component';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {YouTubePlayerModule} from '@angular/youtube-player';
+import {VideoBannerComponent} from './video-banner/video-banner.component';
+import {VideoItemComponent} from './video-item/video-item.component';
+import {VideoListingComponent} from './video-listing/video-listing.component';
+import {RouterModule} from '@angular/router';
+import {HomeBaseComponent} from './home-base/home-base.component';
 
 const DECLARATIONS = [
+  HomeBaseComponent,
   VideoBannerComponent,
   VideoListingComponent,
   VideoItemComponent,
@@ -13,7 +16,20 @@ const DECLARATIONS = [
 
 @NgModule({
   declarations: DECLARATIONS,
-  imports: [CommonModule, YouTubePlayerModule],
+  imports: [CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomeBaseComponent
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+      }
+    ]),
+    YouTubePlayerModule],
   exports: DECLARATIONS,
 })
-export class HomeModule {}
+export class HomeModule {
+}
