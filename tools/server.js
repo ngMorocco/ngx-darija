d88c6ec9279555eb64ae9d432b77b81630da3eca
Slimane IKOURFALN <slimane.ikourfaln@outlook.com>
2021-03-19 22:50:16 +0100

    feat(tools): add root to get video detail using functions

--- tools/server.js
+++ tools/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const playlist = require('../src/functions/playlist');
+const videos = require('../src/functions/videos');
 const cors = require('cors')
 
 const lang = process.argv[2] || 'en';
@@ -12,6 +13,11 @@ const dir = path.join(__dirname, '..', 'dist', lang);
 app.get('**/.netlify/functions/playlist', cors(), async (_, res) => {
     const response = await playlist.handler();
     res.status(response.statusCode).send(response.body);
+});
+
+app.get('**/.netlify/functions/videos/:videoId', cors(), async (req, res) => {
+  const response = await videos.handler(req.params.videoId);
+  res.status(response.statusCode).send(response.body);
 });
 
 console.log(`🚀 Serving files from ${dir} 🚀`);
