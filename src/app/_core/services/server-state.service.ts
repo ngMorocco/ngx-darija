e694f74e974e9fc2ff6c90dacb67e928b154c0be
Chihab Otmani <chihab@gmail.com>
2021-03-23 08:13:17 +0100

    chore: replace tslint with eslint (#35)
    * chore: replace tslint with eslint
    * refactor: fix liniting issues

--- src/app/_core/services/server-state.service.ts
+++ src/app/_core/services/server-state.service.ts
@@ -10,13 +10,12 @@ export class ServerStateService {
   constructor(private state: TransferState) {}
   hydrate(key: string): MonoTypeOperatorFunction<any> {
     const STATE_KEY_ITEMS = makeStateKey(key);
-    return (obs: Observable<any>) => {
-      return obs.pipe(
+    return (obs: Observable<any>) =>
+      obs.pipe(
         tap(videos => {
           this.state.set(STATE_KEY_ITEMS, videos);
         }),
         startWith(this.state.get(STATE_KEY_ITEMS, [] as any))
       );
-    };
   }
 }
