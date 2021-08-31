ea2f5750b0ed0757aa9bb3b1f4c2afff19e2f90c
Chihab Otmani <chihab@gmail.com>
2021-08-31 12:16:21 +0100

    feat: add contribute route

--- src/app/playlist/playlist-routing.module.ts
+++ src/app/playlist/playlist-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     children: [
       {
         path: '',
+        redirectTo: 'contribute',
+        pathMatch: 'full'
+      },
+      {
+        path: 'contribute',
         component: ContributeComponent
       },
       {
