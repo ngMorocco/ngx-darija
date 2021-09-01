058fcf28aafd505cb325571bbdd460405e0c690d
Chihab Otmani <chihab@gmail.com>
2021-09-01 13:10:55 +0100

    feat: add event section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { IntroComponent } from './intro/intro.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NewsletterModule } from '@shared/components/newsletter/newsletter.module';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
-  declarations: [HomeComponent, IntroComponent],
+  declarations: [HomeComponent, IntroComponent, EventComponent],
   imports: [CommonModule, HomeRoutingModule, NewsletterModule]
 })
 export class HomeModule {}
